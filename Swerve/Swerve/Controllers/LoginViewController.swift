3cfcfe79140ba063f0aff27ed2d6d1793d592c99
Daniel Harris <09harrisda@gmail.com>
2018-07-24 16:49:45 -0700

    Added initial loginViewController

---
+++ Swerve/Swerve/Controllers/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  Swerve
+//
+//  Created by Daniel Harris on 24/07/2018.
+//  Copyright © 2018 Daniel Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loginButton.layer.masksToBounds = true
+        loginButton.layer.cornerRadius = loginButton.frame.width / 2
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        print("login button tapped")
+    }
+    
+}
+
